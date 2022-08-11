d012c776894c74685d2b02a74c698d9eee9391fb
shengzhiying-Luo <57742156+shengzhiying-Luo@users.noreply.github.com>
2022-08-11 17:55:47 +0800

    Update main.py
    1

--- main.py
+++ main.py
@@ -53,6 +53,6 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 love_code = get_love_code()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()},"love_code":{"value":"love_code"}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()},"love_code":{"value":love_code}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
